ff29dca9f1cd415e2d8d6c8cfc2e2bca4e78732a
hailey.forar <hailey.forar@MU-C1-01.mullen.pvt.k12.co.us>
2020-02-13 11:52:23 -0700

    Still working - added txt files

--- Monopoly/src/monopoly/MonopolyRunner.java
+++ Monopoly/src/monopoly/MonopolyRunner.java
@@ -7,8 +7,17 @@ public class MonopolyRunner
 	
 	public static void main(String[] args)
 	{
+		introToGame();
+		rulesOfTheGame();
 		
 		
-		
+	}
+	
+	public static void introToGame(){
+		Introduction.introToGame();
+	}
+	
+	public static void rulesOfTheGame() {
+		Introduction.rules();
 	}
 }
