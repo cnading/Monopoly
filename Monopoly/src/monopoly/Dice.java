8998e181ee3e96ea0b280a986166cbdd218270eb
cole.nading <cole.nading@MU-C1-10.mullen.pvt.k12.co.us>
2020-02-13 11:39:14 -0700

    diceeeee

---
+++ Monopoly/src/monopoly/Dice.java
@@ -0,0 +1,14 @@
+package monopoly;
+
+public class Dice
+	{
+		
+		public static int rollDice()
+			{
+				int die1 = (int) (Math.random() * 6) + 1;
+				int die2 = (int) (Math.random() * 6) + 1;
+				int total = die1+die2;
+				return total;
+			}
+
+	}
